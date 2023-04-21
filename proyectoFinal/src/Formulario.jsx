5f1e48e5f6cb028bac81e0e413567b64cbafe455
reds98 <sahid.rojas64@gmail.com>
2023-04-20 20:32:01 -0600

    Agregue el formulario basico pero falta agregar los demas campos

--- proyectoFinal/src/Formulario.jsx
+++ proyectoFinal/src/Formulario.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Formulario.css'
 export default function Formulario() {
-  return (
-    <div>Formulario</div>
-  )
+    const [pacientes, setPacientes] = useState([])
+    const [mascota, setMascota] = useState("")
+    const [dueño, setDueño] = useState("")
+
+    function agregarPaciente(){
+      const informacion={
+        mascota,
+        dueño
+      }
+      setPacientes([...pacientes,informacion])
+      setMascota("")
+      setDueño("")
+    }
+
+
+
+    return (
+        <div>
+            <form className='inputForm' action="">
+                <label htmlFor="mascota">Nombre Mascota </label>
+                <input id='mascota' onChange={(e) => { setMascota(e.target.value) }} value={mascota} type="text" />
+                <label htmlFor="">Nombre Dueño</label>
+                <input onChange={(e) => { setDueño(e.target.value) }} value={dueño} type="text" />
+                
+                <button onClick={agregarPaciente} type='button'>Agregar</button>
+            </form>
+        </div>
+    )
 }
+ 
